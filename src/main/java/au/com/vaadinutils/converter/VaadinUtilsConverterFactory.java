044523fa34f09f750cfb0dbe3dc1f347a60a028b
Les Corson <lcorson@carmacloud.com>
2022-02-07 10:45:53 +1100

    CAR-4610: Log when factory is created.

--- src/main/java/au/com/vaadinutils/converter/VaadinUtilsConverterFactory.java
+++ src/main/java/au/com/vaadinutils/converter/VaadinUtilsConverterFactory.java
@@ -10,11 +10,16 @@ public class VaadinUtilsConverterFactory extends DefaultConverterFactory {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	public VaadinUtilsConverterFactory() {
+	    System.out.println("Setting up Converter Factory...");
+	}
 
 	@SuppressWarnings("unchecked")
 	@Override
     protected <PRESENTATION, MODEL> Converter<PRESENTATION, MODEL> findConverter(
             Class<PRESENTATION> presentationType, Class<MODEL> modelType) {
+	    
         // Handle String <-> BigDecimal
         if (presentationType == String.class && modelType == BigDecimal.class) {
             return (Converter<PRESENTATION, MODEL>) new StringToBigDecimalConverter();
