5bcfd73113ce13a64d967e474dc1f953ee03c33d
Les Corson <lcorson@carmacloud.com>
2023-08-04 13:55:04 +1000

    CAR-5019: Change Auto Complete Text Field to use popup addon.

--- src/main/java/au/com/vaadinutils/flow/ui/AutoCompleteTextField.java
+++ src/main/java/au/com/vaadinutils/flow/ui/AutoCompleteTextField.java
@@ -6,16 +6,30 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.listbox.ListBox;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.common.base.Preconditions;
+import com.vaadin.componentfactory.Popup;
+import com.vaadin.flow.component.AbstractField.ComponentValueChangeEvent;
+import com.vaadin.flow.component.BlurNotifier.BlurEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.HasValue.ValueChangeListener;
+import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 
-public class AutoCompleteTextField<E> extends TextField {
+import au.com.vaadinutils.flow.helper.VaadinHelper;
+
+public class AutoCompleteTextField<E> extends Div {
 
     private static final long serialVersionUID = -6634513296678504250L;
-    private final Dialog dialog = new Dialog();
-    private final ListBox<String> list = new ListBox<String>();
+    private final Logger logger = LogManager.getLogger();
+    private final TextField field = new TextField();
+    private final Popup popup = new Popup();
     private final Map<String, E> options = new LinkedHashMap<>();
     private AutoCompleteQueryListener<E> listener;
     private AutoCompleteOptionSelected<E> optionListener;
@@ -60,11 +74,16 @@ public class AutoCompleteTextField<E> extends TextField {
      */
 
     public AutoCompleteTextField(final String fieldCaption, final String listCaption) {
-        setClassName(fieldCaption);
-        dialog.add(list);
+        Preconditions.checkNotNull(listCaption, "Field Caption is required to link the popup to the field.");
+        field.setClassName(fieldCaption);
+        field.setId(listCaption);
+        popup.setFor(listCaption);
+        
+        add(field, popup);
 
-        setValueChangeMode(ValueChangeMode.LAZY);
-        addValueChangeListener(valueChangeListener -> {
+        // Set as Lazy and if also set, there can be a timeout value.
+        field.setValueChangeMode(ValueChangeMode.LAZY);
+        field.addValueChangeListener(valueChangeListener -> {
             options.clear();
 
             if (listener != null) {
@@ -85,17 +104,25 @@ public class AutoCompleteTextField<E> extends TextField {
             listItems.add(option.getKey());
         }
 
-        list.setItems(listItems);
-        dialog.open();
-        list.addValueChangeListener(value -> {
-            if (value.isFromClient()) {
-                final E key = options.get(value.getValue());
-                if (key != null) {
-                    optionListener.optionSelected(AutoCompleteTextField.this, key);
-                }
-                dialog.close();
-            }
-        });
+        popup.show();
+        popup.removeAll();
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setWidth("120px");
+        for (String string : listItems) {
+            final Span span = new Span(
+                    new Html("<font color='" + VaadinHelper.CARMA_DARK_BLACK + "'>" + string + "</font>"));
+            span.setId(string);
+            layout.add(span);
+            span.addClickListener(e -> {
+                logger.info("Clicking on " + span.getId().orElse("No ID"));
+                optionListener.optionSelected(AutoCompleteTextField.this, options.get(string));
+                // Clear list and hide
+                popup.removeAll();
+                popup.hide();
+            });
+        }
+        
+        popup.add(layout);
     }
 
     public void setOptionSelectionListener(AutoCompleteOptionSelected<E> listener) {
@@ -118,11 +145,24 @@ public class AutoCompleteTextField<E> extends TextField {
         options.put(optionLabel, option);
     }
 
-    public ListBox<String> getList() {
-        return list;
+    public TextField getField() {
+        return this.field;
+    }
+
+    public void addValueChangeListener(final
+            ValueChangeListener<? super ComponentValueChangeEvent<TextField, String>> event) {
+        field.addValueChangeListener(event);
+    }
+    
+    public void addBlurListener(ComponentEventListener<BlurEvent<TextField>> event) {
+        field.addBlurListener(event);
+    }
+    
+    public void setTextChangeTimeout(final int timeout) {
+        field.setValueChangeTimeout(timeout);
     }
 
     public void hideAutoComplete() {
-        dialog.close();
+        popup.hide();
     }
 }
