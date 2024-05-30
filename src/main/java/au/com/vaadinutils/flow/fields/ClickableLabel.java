472dc4f2423b4ac375a7cdfef156554ae9b433be
Les Corson <lcorson-carma@users.noreply.github.com>
2024-05-30 11:48:35 +1000

    CAR-5272: Fix rendering/layout.

--- src/main/java/au/com/vaadinutils/flow/fields/ClickableLabel.java
+++ src/main/java/au/com/vaadinutils/flow/fields/ClickableLabel.java
@@ -3,18 +3,20 @@ package au.com.vaadinutils.flow.fields;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.html.Span;
 
 /**
- * Adds a {@link Label} in to a {@link VerticalLayout} to allow a simple
- * {@link Button} like feature.<br>
+ * Adds a {@link Label} in to a {@link Span} to allow a simple {@link Button}
+ * like feature.<br>
  * Use the Click Listener from the layout to respond to click events.
  */
-@SuppressWarnings("serial")
-public class ClickableLabel extends VerticalLayout {
+public class ClickableLabel extends Span {
+
+    private static final long serialVersionUID = -6393085595535312254L;
     private Component label;
 
     public ClickableLabel() {
+        this.setWidthFull();
     }
 
     public ClickableLabel(final Component value) {
