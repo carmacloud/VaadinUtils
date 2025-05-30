9cd4d633d081504ab80e9b71c90c03543acefa61
Les Corson <lcorson-carma@users.noreply.github.com>
2025-05-30 15:40:01 +1000

    CAR-5280: Code clean-up.

--- src/main/java/au/com/vaadinutils/flow/fields/ComboBoxWithButton.java
+++ src/main/java/au/com/vaadinutils/flow/fields/ComboBoxWithButton.java
@@ -53,7 +53,7 @@ public class ComboBoxWithButton<T> extends HorizontalLayout {
         return button;
     }
 
-    public void setButton(Button button) {
+    public void setButton(final Button button) {
         this.button = button;
     }
 
@@ -62,11 +62,11 @@ public class ComboBoxWithButton<T> extends HorizontalLayout {
     }
 
     public void addValueChangeListener(
-            ValueChangeListener<? super ComponentValueChangeEvent<ComboBox<T>, T>> listener) {
+            final ValueChangeListener<? super ComponentValueChangeEvent<ComboBox<T>, T>> listener) {
         field.addValueChangeListener(listener);
     }
 
-    public void addButtonClickListener(ComponentEventListener<ClickEvent<Button>> listener) {
+    public void addButtonClickListener(final ComponentEventListener<ClickEvent<Button>> listener) {
         button.addClickListener(listener);
     }
 
@@ -74,7 +74,7 @@ public class ComboBoxWithButton<T> extends HorizontalLayout {
         field.setItems(items);
     }
 
-    public void setItems(@SuppressWarnings("unchecked") T... items) {
+    public void setItems(@SuppressWarnings("unchecked") final T... items) {
         field.setItems(items);
     }
 
