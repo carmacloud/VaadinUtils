99fdf29e0e14e7013e50faadbebb070d05372635
Les Corson <lcorson-carma@users.noreply.github.com>
2025-08-01 15:03:29 +1000

    CAR-5131: Change to use grid as target and open on click for component menus.

--- src/main/java/au/com/vaadinutils/flow/fields/contextmenu/GridContextMenu.java
+++ src/main/java/au/com/vaadinutils/flow/fields/contextmenu/GridContextMenu.java
@@ -2,12 +2,14 @@ package au.com.vaadinutils.flow.fields.contextmenu;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.grid.Grid.Column;
 import com.vaadin.flow.function.SerializablePredicate;
 import com.vaadin.flow.shared.Registration;
 
@@ -17,55 +19,34 @@ import elemental.json.JsonObject;
 public class GridContextMenu<E> extends EntityContextMenu<E> {
 
     private static final long serialVersionUID = -5882295471669681116L;
+
     private final Logger logger = LogManager.getLogger();
     private boolean loadCrud = false;
     private SerializablePredicate<E> dynamicContentHandler;
+
+    // Used to determine which menu is opened or hidden.
+    private ContextType contextType;
+    private int buttonClicked = -1;
+    private AtomicReference<Column<E>> columnClicked = new AtomicReference<Grid.Column<E>>();
 
     // Menu clean-up
     protected Registration reg;
     protected final List<Registration> registrations = new ArrayList<>();
     private int count = 0;
 
-    public GridContextMenu() {
-    }
-
     /**
-     * A replacement for the component context menu which loaded a menu item for
-     * each row, and would thus consume excessive resources if not cleaned up.<br>
-     * This version needs only 1 menu to be added, the click listener and Javascript
-     * to the rest.
+     * Assigns this as the context menu of given Grid. This allows left-click to
+     * open the menu. The context menu only appears on rows in the Grid.
      * 
-     * @param grid            A Grid to use the item click listener to find the
-     *                        record that is to be used.
-     * @param invisibleTarget A {@link Div} component hidden on the screen that acts
-     *                        as the anchor target for popping up the context menu.
+     * @param grid     The {@link Grid} the menu is to be attached to.
+     * @param loadCrud If set to false, do not load the crud from the DB.<br>
+     *                 (Meant for grids backed by a Stored Procedure)
      */
-    public void setAsIconContextMenu(final Grid<E> grid, final Div invisibleTarget) {
-        super.setTarget(invisibleTarget);
+    public void setAsComponentContextMenu(final Grid<E> grid, final boolean loadCrud) {
+        this.contextType = ContextType.LEFT_CLICK;
         setOpenOnClick(true);
 
-        reg = grid.addItemClickListener(e -> {
-            final E selectedItem = e.getItem();
-            if (loadCrud) {
-                setTargetEntity(loadEntity(selectedItem));
-            } else {
-                setTargetEntity(selectedItem);
-            }
-            grid.select(getTargetEntity());
-
-            if (e.getButton() == 0 && GridExtender.ACTION_MENU.equals(e.getColumn().getKey())) {
-                final int x = e.getClientX();
-                final int y = e.getClientY();
-                logger.info(x + " - " + y);
-                // Move the invisible target to the mouse position
-                invisibleTarget.getElement().executeJs("this.style.left = $0 + 'px'; this.style.top = $1 + 'px';", x,
-                        y);
-                // Simulate a click to open the context menu
-                invisibleTarget.getElement().executeJs("this.click();");
-            }
-        });
-        registrations.add(reg);
-        logger.debug("Adding Registration: " + this.getClass().getSimpleName());
+        defaultContextActions(grid, loadCrud);
     }
 
     /**
@@ -77,18 +58,34 @@ public class GridContextMenu<E> extends EntityContextMenu<E> {
      * @param grid     The {@link Grid} the menu is to be attached to.
      */
     public void setAsGridContextMenu(final Grid<E> grid, final boolean loadCrud) {
+        this.contextType = ContextType.GRID;
+
+        defaultContextActions(grid, loadCrud);
+    }
+
+    private void defaultContextActions(final Grid<E> grid, final boolean loadCrud) {
         super.setTarget(grid);
+        this.loadCrud = loadCrud;
+
+        grid.addItemClickListener(e -> {
+            buttonClicked = e.getButton();
+            columnClicked.set(e.getColumn());
+        });
 
         // Only allow context on the rows, not headers or footers.
         setDynamicContentHandler(record -> {
             return record != null;
         });
 
-        this.loadCrud = loadCrud;
-
         reg = addOpenedChangeListener(event -> {
-            grid.select(getTargetEntity());
+            if (event.isOpened()) {
+                // Commented out. Was used when when left-click target was another component.
+                // Leave it in if needed to overcome grid selection issues on left-click with
+                // target as the grid.
+//                    grid.select(getTargetEntity());
+            }
         });
+
         registrations.add(reg);
         logger.debug("Adding Registration: " + this.getClass().getSimpleName());
     }
@@ -104,6 +101,27 @@ public class GridContextMenu<E> extends EntityContextMenu<E> {
     @SuppressWarnings("unchecked")
     @Override
     protected boolean onBeforeOpenMenu(final JsonObject eventDetail) {
+        // Depending on which method is used to show a menu, decide which column is
+        // selected and button clicked to show or hide a menu.
+        boolean showMenu = true;
+        final String column = Optional.ofNullable(columnClicked.get()).map(col -> col.getKey()).orElse(null);
+        switch (contextType) {
+        case GRID:
+            showMenu = buttonClicked != 0 && !GridExtender.ACTION_MENU.equals(column);
+            break;
+        case LEFT_CLICK:
+            showMenu = buttonClicked == 0 && GridExtender.ACTION_MENU.equals(column);
+        default:
+            break;
+        }
+
+        // Reset these to 'not selected'. Will only be set on a left-click.
+        buttonClicked = -1;
+        columnClicked.set(null);
+
+        if (!showMenu) {
+            return false;
+        }
         if (getTarget() instanceof Grid) {
             final Grid<E> grid = (Grid<E>) getTarget();
             final String key = eventDetail.getString("key");
@@ -114,6 +132,7 @@ public class GridContextMenu<E> extends EntityContextMenu<E> {
                     if (item == null) {
                         return true;
                     }
+
                     if (loadCrud) {
                         setTargetEntity(loadEntity(item));
                     } else {
